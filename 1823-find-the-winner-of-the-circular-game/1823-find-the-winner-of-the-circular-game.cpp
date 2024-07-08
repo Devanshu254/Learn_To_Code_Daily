6065c51b55991583d183c4056276f01dc33c1e2c
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-07-08 21:12:57 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (49.85%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        // Initialize an array.
+        vector<int> arr;
+        // Fill the array with i values.
+        for(int i=0;i<n;i++) {
+            arr.push_back(i+1);
+        }
+        int index = 0;
+        while(arr.size() > 1) {
+            // Calculate the index which is to be erased.
+            index = (index + (k-1))% arr.size();
+            // Erase the index.
+            arr.erase(arr.begin() + index);
+        }
+        // Return the first element of the list.
+        return arr[0];
+    }
+};
