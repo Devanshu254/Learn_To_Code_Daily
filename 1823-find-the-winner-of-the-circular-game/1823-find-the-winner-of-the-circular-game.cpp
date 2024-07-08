50c2937761750e8f86e32553976bd2bf565cdf0c
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-07-08 21:22:06 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (49.85%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        // Initialize an array.
+        // Initialize an array. 
         vector<int> arr;
-        // Fill the array with i values.
+        // Fill the array with i values. 
         for(int i=0;i<n;i++) {
             arr.push_back(i+1);
         }
@@ -18,3 +18,5 @@ public:
         return arr[0];
     }
 };
+// Time Complexity: O(N) + O(N) * O(N) = O(N^2).
+// Space Complexity: O(N) -> Using an extra array.
