76e553dcf5b068035a0f3081bdaa428ab75d2b50
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-07-10 16:46:08 +0530

    Time: 59 ms (33.06%), Space: 49.4 MB (52.07%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.js
+++ 1598-crawler-log-folder/1598-crawler-log-folder.js
@@ -1,19 +1,19 @@
-class Solution {
-public:
-    int minOperations(vector<string>& logs) {
-        int level = 0;
-        for(int i=0;i<logs.size();i++) {
-            const string& log = logs[i];
-            if(log == "../") {
-                if(level != 0) {
-                    level--;
-                }
-            }else if(log == "./") {
-                continue;
-            }else {
-                level++;
+/**
+ * @param {string[]} logs
+ * @return {number}
+ */
+var minOperations = function(logs) {
+    let level = 0;
+    for(const log of logs) {
+        if(log == "../") {
+            if(level != 0) {
+                level--;
             }
+        }else if(log == "./") {
+            continue;
+        }else {
+            level++;
         }
-        return level;
     }
+    return level;
 };
