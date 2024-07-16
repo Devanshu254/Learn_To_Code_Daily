52573f6ce07082c6339d7f41a760ea88f1897430
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-07-16 11:19:39 +0530

    Time: 48 ms (91.94%), Space: 50 MB (53.13%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let n = numbers.length;
+    let i=0, j=n-1;
+    while(i<j) {
+        if(numbers[i] + numbers[j] > target) {
+            j--;
+        }else if(numbers[i] + numbers[j] < target) {
+            i++;
+        }else {
+            return [i+1, j+1];
+        }
+    }
+    return [0];
+};
