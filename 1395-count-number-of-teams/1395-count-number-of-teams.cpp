2f858328a10b2c4e201453d00fdb9bb268d57e97
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-07-29 16:31:11 +0530

    Time: 29 ms (74.39%), Space: 11.7 MB (40.04%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
+        int teams = 0;
         int n = rating.size();
-        int count = 0;
-        for(int i=0;i<n;i++) {
-            for(int j=i+1;j<n;j++) {
-                for(int k=j+1;k<n;k++) {
-                    if(rating[i]>rating[j] && rating[j]>rating[k] ||
-                    rating[i]<rating[j] && rating[j]<rating[k]) {
-                        count++;
-                    }
+        for(int j=1;j<n-1;j++) {
+            int countSmallerLeft = 0, countLargerRight = 0;
+            int countLargerLeft = 0, countSmallerRight = 0;
+            for(int i=0;i<j;i++) {
+                if(rating[i] < rating[j]) {
+                    countSmallerLeft++;
+                }
+                if(rating[i] > rating[j]) {
+                    countLargerLeft++;
                 }
             }
+            for(int k=j+1;k<n;k++) {
+                if(rating[j] < rating[k]) {
+                    countLargerRight++;
+                }
+                if(rating[j] > rating[k]) {
+                    countSmallerRight++;
+                }
+            }
+            teams += countSmallerLeft * countLargerRight;
+            teams += countLargerLeft * countSmallerRight;
         }
-        return count;
+        return teams;
     }
 };
